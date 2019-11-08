ca23b428fe3cbcfa8a7c7b2138569757effe1d70
Tanner Olsen [Student] <tanner.olsen@student.sl.on.ca>
2019-11-08 13:39:37 -0500

    added in wall jumping

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -26,6 +26,8 @@ public class SoyBoyController : MonoBehaviour
 
     public float airAccel = 3f;
 
+    public float jump = 14f;
+
 
     void Awake()
     {
@@ -65,6 +67,55 @@ public class SoyBoyController : MonoBehaviour
         else
         {
             return false;
+        }
+    }
+
+    public bool IsWallToLeftOrRight()
+    {
+        bool wallOnleft = Physics2D.Raycast(new Vector2(transform.position.x - width, transform.position.y), -Vector2.right, rayCastLengthCheck);
+        bool wallOnRight = Physics2D.Raycast(new Vector2(transform.position.x + width, transform.position.y), Vector2.right, rayCastLengthCheck);
+
+        if (wallOnleft || wallOnRight)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool PlayerIsTouchingGroundOrWall()
+    {
+        if (PlayerIsOnGround() || IsWallToLeftOrRight())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int GetWallDirection()
+    {
+        bool isWallLeft = Physics2D.Raycast(new Vector2(
+        transform.position.x - width, transform.position.y),
+        -Vector2.right, rayCastLengthCheck);
+        bool isWallRight = Physics2D.Raycast(new Vector2(
+        transform.position.x + width, transform.position.y),
+        Vector2.right, rayCastLengthCheck);
+        if (isWallLeft)
+        {
+            return -1;
+        }
+        else if (isWallRight)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
         }
     }
 
@@ -119,7 +170,7 @@ public class SoyBoyController : MonoBehaviour
         }
 
         var xVelocity = 0f;
-        // 2
+
         if(PlayerIsOnGround() && input.x == 0)
         {
             xVelocity = 0f;
@@ -128,11 +179,27 @@ public class SoyBoyController : MonoBehaviour
         {
             xVelocity = rb.velocity.x;
         }
-        // 3
+
+        var yVelocity = 0f;
+        if (PlayerIsTouchingGroundOrWall() && input.y == 1)
+        {
+            yVelocity = jump;
+        }
+        else
+        {
+            yVelocity = rb.velocity.y;
+        }
+
+
         rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x)
         * acceleration, 0));
-        // 4
-        rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+
+        rb.velocity = new Vector2(xVelocity, yVelocity);
+
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround() && input.y == 1)
+        {
+            rb.velocity = new Vector2(-GetWallDirection() * speed * 0.75f, rb.velocity.y);
+        }
 
         if (isJumping && jumpDuration < jumpDurationThreshold)
         {
